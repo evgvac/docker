8d3bc18dd6a42265f559b8d0472e8c15061be277
evgvac <joniy@yandex.ru>
2023-03-28 17:55:09 +0300

    временный коммит

---
+++ test_run/index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello From Kubernetes</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>$USER","<br><p>";
+echo "<b><font color=magenta>Application Version 2.0</b>";
+?>
